cd009c51aa79b23f0a65e07f604572178b9b12ed
git config --global user.name TNorsang <thenorsang@gmail.com>
2019-01-18 23:53:30 -0500

    added main.js index.html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -12,7 +12,46 @@ var config = {
   };
   firebase.initializeApp(config);
 
+  var database = firebase.database();
 
+  function addTrain() {
+    //   event.preventDefault();
+
+// ------ Saves the value inserted in the text box ------  \\
+      var trainName = $('#trainName').val().trim();
+      var destination = $('#destination').val().trim();
+      var frequency = $('#frequency').val().trim();
+      var nextArrival = $('#nextArrival').val().trim();
+      var minutesAway = $('#minutesAway').val().trim();
+  
+// ------ A local storage to keep the information ------  \\
+  var addedTrain = {
+      name: trainName,
+      dest: destination,
+      freq: frequency,
+      nxtArr: nextArrival,
+      minAw: minutesAway
+  };
+
+  console.log(addedTrain);
+  database.ref().push(addedTrain);
+// ------ Clearing the data inside the text boxes ------  \\
+  $('#trainName').val('');
+  $('#destination').val('');
+  $('#frequency').val('');
+  $('#nextArrival').val('');
+  $('#minutesAway').val('');
+
+  }
+  
+  addTrain();
+
+// var tFrequency = 3;
+
+// var firstTime = "03:30";
+
+// var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+// console.log(firstTimeConverted)
 
 
 
