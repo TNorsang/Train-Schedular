2063847a871c933353f259f7eb4a789dd9217a5f
Norsang <thenorsang@gmail.com>
2019-01-19 15:20:58 -0500

    added main.js index.html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,8 @@
 // --------- Wait's for the Document in HTML to load
-$(document).ready( function() {
-    
-// Initialize Firebase
-var config = {
+$(document).ready(function () {
+
+  // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyDr4u0eBzVMZ47fIwP-JTEApEyWp_0S8vE",
     authDomain: "train-scheduler-bcc29.firebaseapp.com",
     databaseURL: "https://train-scheduler-bcc29.firebaseio.com",
@@ -14,52 +14,82 @@ var config = {
 
   var database = firebase.database();
 
-  function addTrain() {
-      
 
-// ------ Saves the value inserted in the text box ------  \\
-      var trainName = $('#trainName').val().trim();
-      var destination = $('#destination').val().trim();
-      var frequency = $('#frequency').val().trim();
-      var nextArrival = $('#nextArrival').val().trim();
-      var minutesAway = $('#minutesAway').val().trim();
+  database.ref('newlist').on("child_added", function (childSnapshot) {
+    // Log everything that's coming out of snapshot
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().dest);
+
+  });
+
+  $('.submit').on('click', function (event) {
+    event.preventDefault();
+    // grabs the text input in the text field
+    var trainName = $('#trainName').val().trim();
+    var destination = $('#destination').val().trim();
+    var frequency = $('#frequency').val().trim();
   
-// ------ A local storage to keep the information ------  \\
-  var addedTrain = {
+
+    console.log(trainName);
+
+    // ------ A local storage to keep the information ------  \\
+    var addedTrain = {
       name: trainName,
       dest: destination,
-      freq: frequency,
-      nxtArr: nextArrival,
-      minAw: minutesAway
-  };
+      freq: frequency
+    };
 
-  console.log(addedTrain);
-  database.ref().push(addedTrain);
+    console.log(addedTrain);
+    database.ref('newlist').push(addedTrain);
 
-// ------ Clearing the data inside the text boxes ------  \\
+    // Clearing the data inside the text boxes 
   $('#trainName').val('');
   $('#destination').val('');
   $('#frequency').val('');
   $('#nextArrival').val('');
   $('#minutesAway').val('');
 
-  }
+  });
+
+
+
+
+
+
   
-  addTrain();
-
-// var tFrequency = 3;
-
-// var firstTime = "03:30";
-
-// var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-// console.log(firstTimeConverted)
 
 
 
 
+  //  // Assumptions
+  //  var tFrequency = 3;
 
+  //  // Time is 3:30 AM
+  //  var firstTime = "03:30";
 
+  //  // First Time (pushed back 1 year to make sure it comes before current time)
+  //  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  //  console.log(firstTimeConverted);
 
+  //  // Current Time
+  //  var currentTime = moment();
+  //  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+  //  // Difference between the times
+  //  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  //  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  //  // Time apart (remainder)
+  //  var tRemainder = diffTime % tFrequency;
+  //  console.log(tRemainder);
+
+  //  // Minute Until Train
+  //  var tMinutesTillTrain = tFrequency - tRemainder;
+  //  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+  //  // Next Train
+  //  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  //  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 
 
